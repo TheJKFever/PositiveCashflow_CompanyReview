5986ce7e0d38853592711a9dc574661061ab68a7
Jeroen Goossens <jgem12345@gmail.com>
2013-07-25 17:40:00 -0400

    LLString commit

---
+++ src/baseClasses/StringLogInterface.java
@@ -0,0 +1,10 @@
+package baseClasses;
+
+
+public interface StringLogInterface {
+	public void insert(String element);
+	public void clear();
+	public boolean isFull();
+	public boolean contains(String element);
+	public String toString();
+}
