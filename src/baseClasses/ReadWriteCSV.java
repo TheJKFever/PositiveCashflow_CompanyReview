4b9d3380542aee576205b5f6c5cdb5ac6bd4be54
Jeroen Goossens <jgem12345@gmail.com>
2013-07-30 14:21:11 -0400

    push

--- src/baseClasses/ReadWriteCSV.java
+++ src/baseClasses/ReadWriteCSV.java
@@ -1,5 +1,10 @@
 package baseClasses;
 
 public class ReadWriteCSV {
-	
+	CSVReader reader = new CSVReader(new FileReader("yourfile.csv"));
+    String [] nextLine;
+    while ((nextLine = reader.readNext()) != null) {
+        // nextLine[] is an array of values from the line
+        System.out.println(nextLine[0] + nextLine[1] + "etc...");
+    }
 }
