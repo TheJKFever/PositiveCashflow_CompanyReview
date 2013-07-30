86cfad15382609ac66793a8852c063e465159975
Jeroen Goossens <jgem12345@gmail.com>
2013-07-30 14:40:24 -0400

    CSV works

--- src/baseClasses/ReadWriteCSV.java
+++ src/baseClasses/ReadWriteCSV.java
@@ -1,10 +1,50 @@
 package baseClasses;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+
 public class ReadWriteCSV {
-	CSVReader reader = new CSVReader(new FileReader("yourfile.csv"));
-    String [] nextLine;
-    while ((nextLine = reader.readNext()) != null) {
-        // nextLine[] is an array of values from the line
-        System.out.println(nextLine[0] + nextLine[1] + "etc...");
-    }
+
+	public void read(String input){
+		CSVReader reader = null;
+		try {
+			reader = new CSVReader(new FileReader(input));
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		String [] nextLine = null;
+		try {
+			while ((nextLine = reader.readNext()) != null) {
+				// nextLine[] is an array of values from the line
+				System.out.println(nextLine[0] + nextLine[1] + "etc...");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void write(String[] output, String adress){
+		CSVWriter writer = null;
+		try {
+			writer = new CSVWriter(new FileWriter("yourfile.csv"), '\t');
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	     // feed in your array (or convert your data to an array)
+	     String[] entries = "first#second#third".split("#");
+	     writer.writeNext(entries);
+		try {
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
