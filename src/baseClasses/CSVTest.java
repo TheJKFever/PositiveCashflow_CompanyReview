f3237256ad07d4e20d6826430b377257af2b4ff7
Jeroen Goossens <jgem12345@gmail.com>
2013-07-30 15:23:05 -0400

    update

--- src/baseClasses/CSVTest.java
+++ src/baseClasses/CSVTest.java
@@ -10,26 +10,29 @@ import au.com.bytecode.opencsv.CSVWriter;
 
 public class CSVTest {
 
-private static final String ADDRESS_FILE="addresses.csv";
+private static final String ADDRESS_FILE="transactions.csv";
 	
 	public static void main(String[] args) throws IOException {
 		
 		CSVReader reader = new CSVReader(new FileReader(ADDRESS_FILE));
 		String [] nextLine;
+		
+//		System.out.println("first line consists of " + reader.readNext()[0] + " and "  +  reader.readNext()[1] + " and "  +reader.readNext()[2]);
+		
 		while ((nextLine = reader.readNext()) != null) {
-			System.out.println("Name: [" + nextLine[0] + "]\nAddress: [" + nextLine[1] + "]\nEmail: [" + nextLine[2] + "]");
+			System.out.println(nextLine[0] + " \t|| " + nextLine[1] + " \t\t\t\t|| " + nextLine[2] );
 		}
 		
 		// Try writing it back out as CSV to the console
-		CSVReader reader2 = new CSVReader(new FileReader(ADDRESS_FILE));
-		List<String[]> allElements = reader2.readAll();
-		StringWriter sw = new StringWriter();
-		CSVWriter writer = new CSVWriter(sw);
-		writer.writeAll(allElements);
-		
-		System.out.println("\n\nGenerated CSV File:\n\n");
-		System.out.println(sw.toString());
-		
+//		CSVReader reader2 = new CSVReader(new FileReader(ADDRESS_FILE));
+//		List<String[]> allElements = reader2.readAll();
+//		StringWriter sw = new StringWriter();
+//		CSVWriter writer = new CSVWriter(sw);
+//		writer.writeAll(allElements);
+//		
+//		System.out.println("\n\nGenerated CSV File:\n\n");
+//		System.out.println(sw.toString());
+//		
 		
 	}
 }
