c4c98379f401de2b354463835e8aae60898fa873
Jon Koehmstedt <Jon.Koehmstedt@gmail.com>
2013-07-30 15:41:36 -0400

    push

--- src/backend/DatabaseBE.java
+++ src/backend/DatabaseBE.java
@@ -8,8 +8,29 @@ public class DatabaseBE {
 	protected SortedLL<TransactionBE> companies;
 
 	public DatabaseBE(){
-		unknown=null;
-		companies=null;
+		unknown = new SortedLL<TransactionBE>();
+		companies = new SortedLL<TransactionBE>();
 	}
+
+	//GETTERS
+	public SortedLL<TransactionBE> getUnknown() {
+		return unknown;
+	}
+
+	public SortedLL<TransactionBE> getCompanies() {
+		return companies;
+	}
+
+	public void writeToFile(SortedLL<TransactionBE> list){
+		//TODO
+	}
+	
+	public SortedLL<TransactionBE> readToFile(){
+		//TODO
+		return null;
+	}
+	
+	
+	
 	
 }
