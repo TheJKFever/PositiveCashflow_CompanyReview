1338e68d546179739227bc52a396001c39e9e9f9
Jon Koehmstedt <Jon.Koehmstedt@gmail.com>
2013-07-31 15:08:10 -0400

    push

--- src/backend/DatabaseBE.java
+++ src/backend/DatabaseBE.java
@@ -1,12 +1,12 @@
 package backend;
 
 import java.io.*;
-
+import java.util.ArrayList;
+import au.com.bytecode.opencsv.CSVReader;
 import baseClasses.*;
 
 public class DatabaseBE implements Serializable {
 
-	private static int lastUniqueID;
 	private static final long serialVersionUID = 1L;
 	protected SortedLL<TransactionBE> unknown;
 	protected SortedLL<TransactionBE> companies;
@@ -16,6 +16,8 @@ public class DatabaseBE implements Serializable {
 		companies = new SortedLL<TransactionBE>();
 	}
 	
+	
+	//TODO Make sure this returns the proper value and doesn't just do nothing
 	public DatabaseBE(String fileName){
 		openDatabase(fileName);
 	}
@@ -42,7 +44,72 @@ public class DatabaseBE implements Serializable {
 		return null;
 	}
 	
-	public boolean isKnown(TransactionBE t){
+	public void addFromFile(String input){
+		//TODO input is a file, read CSV
+		CSVReader reader = null;
+		ArrayList<TransactionBE> transactions = new ArrayList<TransactionBE>();
+		String [] nextLine = null;
+		int indexCompany = -1;
+		int indexType = -1;
+		int indexDescription = -1;
+		int indexGood = -1;
+		try {
+			reader = new CSVReader(new FileReader(input));
+			if((nextLine = reader.readNext()) != null){
+				int length = nextLine.length;
+				for(int i = 0; i < length; i++){
+					if(nextLine[i].equalsIgnoreCase("company")){
+						indexCompany = i;
+					}
+					if(nextLine[i].equalsIgnoreCase("type of company")){
+						indexType = i;
+					}
+					if(nextLine[i].equalsIgnoreCase("good/bad")){
+						indexGood = i;
+					}
+					if(nextLine[i].equalsIgnoreCase("original description")){
+						indexDescription = i;
+					}
+				}
+			}
+			if(indexCompany==-1 || indexType == -1 || indexDescription == -1 || indexGood ==-1){
+				throw new Exception("Formatted incorrectly");
+			}
+			while ((nextLine = reader.readNext()) != null) {
+				// nextLine[] is an array of values from the line
+				TransactionBE newTransaction = new TransactionBE( new Company(nextLine[indexGood], nextLine[indexCompany], );
+				transactions.add(new TransactionBE(nextLine[indexDescription], Double.valueOf(nextLine[indexType]), nextLine[indexCompany]));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return transactions;
+	}
+	
+	public void addTransaction(TransactionBE t){
+		if (!isKnown(t)){
+			if (t.getCompany()==null){
+				if(!isUnknown(t)){
+					unknown.add(t);
+				}				
+			} else {
+				companies.add(t);
+			}
+		}		
+	}	
+	
+	//GETTERS
+	public SortedLL<TransactionBE> getUnknown() {
+		return unknown;
+	}
+
+	public SortedLL<TransactionBE> getCompanies() {
+		return companies;
+	}
+
+	
+	//PRIVATE METHODS
+	private boolean isKnown(TransactionBE t){
 		for (int i=0; i<companies.length();i++){
 			companies.current = companies.head;
 			while(companies.current!=null){
@@ -56,7 +123,7 @@ public class DatabaseBE implements Serializable {
 		return false;
 	}
 	
-	public boolean isUnknown(TransactionBE t) {
+	private boolean isUnknown(TransactionBE t) {
 		for (int i=0; i<unknown.length();i++){
 			unknown.current = unknown.head;
 			while(unknown.current!=null){
@@ -70,32 +137,6 @@ public class DatabaseBE implements Serializable {
 		return false;
 	}
 	
-	public void addFromFile(String input){
-		//TODO
-	}
-		
-	public void addTransaction(TransactionBE t){
-		//TODO
-		if (!isKnown(t)){
-			if (t.getCompany()==null){
-				if(!isUnknown(t)){
-					unknown.add(t);
-				}				
-			} else {
-				companies.add(t);
-			}
-		}		
-	}
 	
-	
-	//GETTERS
-	public SortedLL<TransactionBE> getUnknown() {
-		return unknown;
-	}
-
-	public SortedLL<TransactionBE> getCompanies() {
-		return companies;
-	}
-
 	
 }
